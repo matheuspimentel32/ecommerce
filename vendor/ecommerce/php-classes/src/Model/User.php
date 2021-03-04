c235da39d23179057fb47c9dff88189dfa50892d
Matheus <matheusbpimentel@gmail.com>
2021-03-03 21:05:59 -0300

    Paginaçao de usuarios na tela de administrador

--- vendor/ecommerce/php-classes/src/Model/User.php
+++ vendor/ecommerce/php-classes/src/Model/User.php
@@ -327,6 +327,62 @@ class User extends Model{
 
     }
 
+    public static function getPage($page = 1, $itemsPerPage = 10)
+	{
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_users a
+            INNER JOIN tb_persons b USING(idperson)
+            ORDER BY a.iduser ASC
+			LIMIT $start, $itemsPerPage;
+		");
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) //Ceil arredonda para o próximo número inteiro
+        ];
+
+    }
+    
+
+    public static function getPageSearch($search, $page = 1, $itemsPerPage = 10)
+	{
+
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+		$results = $sql->select("SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_users a
+            INNER JOIN tb_persons b USING(idperson)
+            WHERE b.desperson LIKE :search OR b.desemail = :searchEmail OR a.deslogin LIKE :search
+            ORDER BY a.iduser ASC
+			LIMIT $start, $itemsPerPage;
+		", [
+            ':search'=>'%' . $search . '%',
+            ':searchEmail'=> $search
+        ]);
+
+		$resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+		return [
+			'data'=>$results,
+			'total'=>(int)$resultTotal[0]["nrtotal"],
+			'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemsPerPage) //Ceil arredonda para o próximo número inteiro
+        ];
+
+	}
+
+
+
+
     //Erros do cadastro
     public static function setError($msg)
     {
